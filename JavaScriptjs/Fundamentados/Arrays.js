43f0c9de9729d4146d586a44bc2934e8bc9d65c6
wellington santos <wellinsantos84@gmail.com>
2025-03-23 23:08:31 -0300

    hfd

---
+++ JavaScriptjs/Fundamentados/Arrays.js
@@ -0,0 +1,9 @@
+const Valores = ["Café", "Arros","Bife","Carne", "Peixe", 'Macarrão']
+console.log(Valores[2], Valores[1])
+
+Valores[2] = "Frango"
+console.log(Valores)
+Valores.push("Manga") // add final
+console.log(Valores)
+Valores.pop()
+console.log(Valores)
---
+++ JavaScriptjs/Fundamentados/Objetos.js
@@ -0,0 +1,16 @@
+const prod1 = {
+    
+
+}
+prod1.nomeCelular = 'Kaiok';
+prod1.Tamanho_Tela= "6.9";
+prod1['Desconto Legal'] = 9.40
+console.log(prod1)
+
+const prod2 = {
+    nome: "Polo",
+    preço: "130.99",
+    cor: 'Azul',
+    'Desconto Legal': 0.20
+}
+console.log(prod2)
