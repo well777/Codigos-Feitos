8e5c27488c36cdb8dfdb981135aafe47b14dee16
wellington santos <wellinsantos84@gmail.com>
2025-04-18 23:01:40 -0300

    dfgv cdsa

--- JavaScriptjs/Organização/1.4.js
+++ JavaScriptjs/Organização/1.4.js
@@ -3,4 +3,4 @@ var tempFahr = parseFloat(prompt("Digite a temperatura em graus Fahrenheit: "));
 var fahrenheitToCelcius = ((tempFahr - 32) * 5 ) / 9;
 
 alert(`A temperatura de ${tempFahr}°F convertida para Celcius é de ${fahrenheitToCelcius.toFixed(2)}°C.`)
-console.log(fahrenheitToCelcius)
+console.log(fahrenheitToCelcius)s
